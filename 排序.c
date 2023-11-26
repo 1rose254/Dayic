565a58be9eb53a4d5e819d4cb8c3cdb2407b4e03
1rose254 <150570531+1rose254@users.noreply.github.com>
2023-11-26 12:50:49 +0800

    Update 排序.c

--- 排序.c
+++ 排序.c
@@ -5,17 +5,20 @@ int com_int(const void*p1,const void*p2)
 }
 qsort(a,sz,sizeof(a[0]),com_int);
 //浮点型快速排序
-int com_float(const void*p1,const void*p2)
+int com_double(const void*p1,const void*p2)
 {
-	return *(float*)p1-*(float*)p2;
+	return *(double*)p1-*(double*)p2;
 }
+qsort(a,sz,sizeof(a[0]),com_double);
 //结构体名字快速排序
 int com_stu_name(const void*p1,const void*p2)
 {
 	return strcmp(((struct stu*)p1)->name,((struct stu*)p2)->name);
 }
+qsort(a,sz,sizeof(a[0]),com_stu_name);
 //结构体年龄快速排序
 int com_stu_age(const void*p1,const void*p2)
 {
 	return ((struct stu*)p1)->age-((struct stu*)p2)->age);
 }
+qsort(a,sz,sizeof(a[0]),com_name_age);
